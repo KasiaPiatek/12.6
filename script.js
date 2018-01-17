52f5ce0d129e7cf80deebe833542e9dd86bafeec
Kasia Piatek <piatekkatarzynaolga@gmail.com>
2018-01-17 22:11:10 +0100

    change script.js

--- script.js
+++ script.js
@@ -5,31 +5,22 @@ $('#search').click(searchCountries);
 
 function searchCountries() {
   	var countryName = $('#country-name').val();
-	if(!countryName.lenght) countryName = 'Poland';
+	if(!countryName.length) countryName = 'Poland';
 $.ajax({
   		url: url + countryName,
   		method: 'GET',
   		success: showCountriesList
   	});
 }
-
 searchCountries();
 
-function showCountriesList(resp) {
-  countriesList.empty();
-}
 
 function showCountriesList(resp) {
     countriesList.empty();
-    resp.forEach(function(item) {
-        //Here is the code that will execute on each successive item in the collection. A single item is hidden under an item variable.
+    resp.forEach(function(item){
+      $('<li>').text(item.name).appendTo(countriesList);
     });
 
+}
 showCountriesList();
 
-resp.forEach(function(item){
-   	$('<li>').text(item.name).appendTo(countriesList);
-});
-
-
-}
